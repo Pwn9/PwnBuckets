df6f9b8a3894467c3710406502c126ffb037933b
Matt Sudol <matt.sudol@anaren.com>
2018-03-07 14:06:13 -0500

    fixing issue with reference in the config loading.

--- src/com/pwn9/PwnBuckets/Config.java
+++ src/com/pwn9/PwnBuckets/Config.java
@@ -7,43 +7,43 @@ public class Config
 	{
 		
 		// Get enabled worlds
-		PwnBuckets.enabledWorlds = getConfig().getStringList("enabled_worlds");
+		PwnBuckets.enabledWorlds = PwnBuckets.instance.getConfig().getStringList("enabled_worlds");
 		
 		// Get water source setting
-		PwnBuckets.blockWaterBucket = getConfig().getBoolean("block_water_bucket", false);
+		PwnBuckets.blockWaterBucket = PwnBuckets.instance.getConfig().getBoolean("block_water_bucket", false);
 		
 		// Get biome bypass settings
-		PwnBuckets.bucketBypass = getConfig().getStringList("bucket_allowed_biomes");
+		PwnBuckets.bucketBypass = PwnBuckets.instance.getConfig().getStringList("bucket_allowed_biomes");
 		
 		// Get water source setting
-		PwnBuckets.blockWaterDispenser = getConfig().getBoolean("block_water_dispenser", false);
+		PwnBuckets.blockWaterDispenser = PwnBuckets.instance.getConfig().getBoolean("block_water_dispenser", false);
 		
 		// Get biome bypass settings
-		PwnBuckets.dispenserBypass = getConfig().getStringList("dispenser_allowed_biomes");
+		PwnBuckets.dispenserBypass = PwnBuckets.instance.getConfig().getStringList("dispenser_allowed_biomes");
 
 		// Get ice melt setting
-		PwnBuckets.blockIceMelt = getConfig().getBoolean("block_ice_melt", false);
+		PwnBuckets.blockIceMelt = PwnBuckets.instance.getConfig().getBoolean("block_ice_melt", false);
 		
 		// Get ice melt bypass settings
-		PwnBuckets.icemeltBypass = getConfig().getStringList("ice_melt_allowed_biomes");
+		PwnBuckets.icemeltBypass = PwnBuckets.instance.getConfig().getStringList("ice_melt_allowed_biomes");
 		
 		// Get lava source setting
-		PwnBuckets.blockLavaBucket = getConfig().getBoolean("block_lava_bucket", false);
+		PwnBuckets.blockLavaBucket = PwnBuckets.instance.getConfig().getBoolean("block_lava_bucket", false);
 		
 		// Get biome lava bypass settings
-		PwnBuckets.lavaBucketBypass = getConfig().getStringList("lava_bucket_allowed_biomes");
+		PwnBuckets.lavaBucketBypass = PwnBuckets.instance.getConfig().getStringList("lava_bucket_allowed_biomes");
 	
 		// Get water source setting
-		PwnBuckets.blockLavaDispenser = getConfig().getBoolean("block_lava_dispenser", false);
+		PwnBuckets.blockLavaDispenser = PwnBuckets.instance.getConfig().getBoolean("block_lava_dispenser", false);
 		
 		// Get biome bypass settings
-		PwnBuckets.lavaDispenserBypass = getConfig().getStringList("lava_dispenser_allowed_biomes");
+		PwnBuckets.lavaDispenserBypass = PwnBuckets.instance.getConfig().getStringList("lava_dispenser_allowed_biomes");
 		
 		// Get ice melt setting
-		PwnBuckets.blockCreativeSource = getConfig().getBoolean("block_creative_source", false);
+		PwnBuckets.blockCreativeSource = PwnBuckets.instance.getConfig().getBoolean("block_creative_source", false);
 		
 		// Get logfile setting
-		PwnBuckets.logEnabled = getConfig().getBoolean("debug_log", false);	
+		PwnBuckets.logEnabled = PwnBuckets.instance.getConfig().getBoolean("debug_log", false);	
 	}
 	
 }
